78c5f907f4b899e300e2f601ebd2992d8946d15b
George <geor2335@stud.kea.dk>
2022-03-31 14:05:15 +0200

    basic api examples

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-import './style.css'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+// page speed insights key:  AIzaSyCJpmbyPCYEITV04onRwPknKcJkdSdEhvU
+
+const url = "https://georgendesign.com/";
+
+async function getCarbonData(url) {
+  const request = await fetch(
+    `https://kea-alt-del.dk/websitecarbon/?url=${url}`
+  );
+  const data = await request.json();
+  console.log(data);
+}
+
+getCarbonData(url);
+
+async function getPageSpeed(url) {
+  const request = await fetch(
+    `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${url}&key=AIzaSyCJpmbyPCYEITV04onRwPknKcJkdSdEhvU`
+  );
+  const data = await request.json();
+  console.log(data);
+}
+
+getPageSpeed(url);
