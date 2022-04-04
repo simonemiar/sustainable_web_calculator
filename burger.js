91531fb27248525de712309da71a8d1cfc55de21
simonemiar <79079392+simonemiar@users.noreply.github.com>
2022-04-04 16:11:00 +0200

    burger menu

---
+++ burger.js
@@ -0,0 +1,10 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', nav)
+function nav(){
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.main-nav');
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('show')
+    })
+}
