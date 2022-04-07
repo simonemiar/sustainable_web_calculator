b2792269fd6c577fe65630f7631bbd91f7067641
George <geor2335@stud.kea.dk>
2022-04-07 13:54:45 +0200

    added js validation

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,3 +1,10 @@
+"use strict";
+
+const introForm = document.querySelector("form");
+const urlInput = document.querySelector("#url_field");
+const errMsg = document.querySelector("#error-msg");
+const sendBtn = document.querySelector(`input[type="submit"]`);
+
 function validURL(str) {
   const pattern = new RegExp(
     "^(https?:\\/\\/)?" + // protocol
@@ -10,3 +17,38 @@ function validURL(str) {
   ); // fragment locator
   return !!pattern.test(str);
 }
+
+function errorMsg(text) {
+  errMsg.classList.remove("hidden");
+  errMsg.textContent = text;
+}
+
+urlInput.addEventListener("input", () => {
+  if (validURL(urlInput.value) || urlInput.value.length === 1) {
+    errMsg.classList.add("hidden");
+  }
+});
+
+urlInput.addEventListener("blur", () => {
+  if (!validURL(urlInput.value) && urlInput.value.length > 0) {
+    errorMsg("This is not a valid URL.");
+  } else if (!urlInput.value) {
+    errorMsg("Please insert an URL here.");
+  } else errMsg.classList.add("hidden");
+});
+
+urlInput.addEventListener("invalid", function (e) {
+  if (e.target.validity.valueMissing) {
+    e.preventDefault();
+
+    urlInput.focus();
+  }
+});
+
+introForm.addEventListener("submit", (e) => {
+  if (!validURL(urlInput.value) && urlInput.value.length > 0) {
+    e.preventDefault();
+    urlInput.focus();
+    urlInput.select();
+  }
+});
