391e8cdb5cfc8790e55c855a71df525252c47f3c
George <geor2335@stud.kea.dk>
2022-04-11 11:29:35 +0200

    broke up js in 2 pages

--- scripts/results.js
+++ scripts/results.js
@@ -1,2 +1,10 @@
-var favoritemovie = sessionStorage.getItem("favoriteMovie");
-console.log(favoritemovie);
+const url = "http://georgendesign.com/";
+
+async function getData() {
+  const request = await fetch(
+    `https://kea-alt-del.dk/websitecarbon/site/?url=${url}`
+  );
+  const data = await request.json();
+}
+
+getData();
